f6655697f94927981e7adf0329f5cc2e3aef7d41
sklep <sklep@tcpower.eu>
2018-01-03 17:26:01 +0100

    Add project files.

---
+++ AdvancedHMI/MainForm.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    '   <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    ' <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.QuickStartLabel = New System.Windows.Forms.Label()
+        Me.ForceItemsIntoToolBox1 = New MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ModbusTCPCom1 = New AdvancedHMIDrivers.ModbusTCPCom(Me.components)
+        Me.DigitalPanelMeter1 = New AdvancedHMIControls.DigitalPanelMeter()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ModbusTCPCom1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(6, 562)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "For Development Source Code Visit" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://www.advancedhmi.com"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'QuickStartLabel
+        '
+        Me.QuickStartLabel.AutoSize = True
+        Me.QuickStartLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.QuickStartLabel.ForeColor = System.Drawing.Color.White
+        Me.QuickStartLabel.Location = New System.Drawing.Point(9, 16)
+        Me.QuickStartLabel.Name = "QuickStartLabel"
+        Me.QuickStartLabel.Size = New System.Drawing.Size(381, 160)
+        Me.QuickStartLabel.TabIndex = 38
+        Me.QuickStartLabel.Text = resources.GetString("QuickStartLabel.Text")
+        Me.QuickStartLabel.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(890, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(322, 47)
+        Me.PictureBox1.TabIndex = 42
+        Me.PictureBox1.TabStop = False
+        '
+        'ModbusTCPCom1
+        '
+        Me.ModbusTCPCom1.DisableSubscriptions = False
+        Me.ModbusTCPCom1.IniFileName = ""
+        Me.ModbusTCPCom1.IniFileSection = Nothing
+        Me.ModbusTCPCom1.IPAddress = "192.168.27.241"
+        Me.ModbusTCPCom1.MaxReadGroupSize = 20
+        Me.ModbusTCPCom1.PollRateOverride = 500
+        Me.ModbusTCPCom1.SwapBytes = True
+        Me.ModbusTCPCom1.SwapWords = False
+        Me.ModbusTCPCom1.TcpipPort = CType(502US, UShort)
+        Me.ModbusTCPCom1.TimeOut = 3000
+        Me.ModbusTCPCom1.UnitId = CType(1, Byte)
+        '
+        'DigitalPanelMeter1
+        '
+        Me.DigitalPanelMeter1.BackColor = System.Drawing.Color.Transparent
+        Me.DigitalPanelMeter1.ComComponent = Me.ModbusTCPCom1
+        Me.DigitalPanelMeter1.DecimalPosition = 0
+        Me.DigitalPanelMeter1.ForeColor = System.Drawing.Color.LightGray
+        Me.DigitalPanelMeter1.KeypadFontColor = System.Drawing.Color.WhiteSmoke
+        Me.DigitalPanelMeter1.KeypadMaxValue = 0R
+        Me.DigitalPanelMeter1.KeypadMinValue = 0R
+        Me.DigitalPanelMeter1.KeypadScaleFactor = 1.0R
+        Me.DigitalPanelMeter1.KeypadText = Nothing
+        Me.DigitalPanelMeter1.KeypadWidth = 300
+        Me.DigitalPanelMeter1.Location = New System.Drawing.Point(13, 16)
+        Me.DigitalPanelMeter1.Name = "DigitalPanelMeter1"
+        Me.DigitalPanelMeter1.NumberOfDigits = 5
+        Me.DigitalPanelMeter1.PLCAddressKeypad = ""
+        Me.DigitalPanelMeter1.PLCAddressValue = "40001"
+        Me.DigitalPanelMeter1.Resolution = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.DigitalPanelMeter1.Size = New System.Drawing.Size(410, 178)
+        Me.DigitalPanelMeter1.TabIndex = 55
+        Me.DigitalPanelMeter1.Text = "DigitalPanelMeter1"
+        Me.DigitalPanelMeter1.Value = 0R
+        Me.DigitalPanelMeter1.ValueScaleFactor = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.DigitalPanelMeter1.ValueScaleOffset = New Decimal(New Integer() {0, 0, 0, 0})
+        '
+        'MainForm
+        '
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1223, 487)
+        Me.Controls.Add(Me.DigitalPanelMeter1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.QuickStartLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.KeyPreview = True
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AdvancedHMI v3.99x"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ModbusTCPCom1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents QuickStartLabel As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents DF1ComWF1 As AdvancedHMIDrivers.SerialDF1forSLCMicroCom
+    Friend WithEvents ForceItemsIntoToolBox1 As MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox
+    Friend WithEvents ModbusTCPCom1 As AdvancedHMIDrivers.ModbusTCPCom
+    Friend WithEvents DigitalPanelMeter1 As AdvancedHMIControls.DigitalPanelMeter
+End Class
